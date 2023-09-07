59a570d3dacc66e665bc656980ecf835743e42d6
varsha0713 <99706376+varsha0713@users.noreply.github.com>
2023-09-07 19:20:36 +0530

    commit

--- 3.py
+++ 3.py
@@ -1,10 +1,9 @@
-n=int(input("n:"))
-
-for i in range(0, n):
-   for j in range(1, i + 1):
-       print("*", end=" ")
-   print()
-for i in range(n, 0, -1):
+for i in range(0, 3):
     for j in range(1, i + 1):
-       print("*", end=" ")
+        print("*", end=" ")
+    print()
+
+for i in range(3, 0, -1):
+    for j in range(1, i + 1):
+        print("*", end=" ")
     print()
