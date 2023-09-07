8d231a9c2d5a85c171a88286670b7c3323b73a2b
varsha0713 <99706376+varsha0713@users.noreply.github.com>
2023-09-07 19:02:14 +0530

    commit

---
+++ 1.py
@@ -0,0 +1,19 @@
+my_array = []
+for i in range(5):
+ num = int(input(f"Enter integer {i + 1}: "))
+ my_array.append(num)
+print("Array items:")
+for item in my_array:
+ print(item)
+//Enter integer 1: 1
+Enter integer 2: 2
+Enter integer 3: 3
+Enter integer 4: 4
+Enter integer 5: 5
+Array items:
+1
+2
+3
+4
+5
+//
